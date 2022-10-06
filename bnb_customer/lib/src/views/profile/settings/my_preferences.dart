ed23229b88dc4f507dab6e6b62a0ca5093342f47
Bikuta-victor1 <adekolavictor51@gmail.com>
2022-10-06 22:08:41 +0100

    Preferences Fixed

--- bnb_customer/lib/src/views/profile/settings/my_preferences.dart
+++ bnb_customer/lib/src/views/profile/settings/my_preferences.dart
@@ -1,6 +1,9 @@
 import 'package:bbblient/src/controller/all_providers/all_providers.dart';
 import 'package:bbblient/src/firebase/customer.dart';
 import 'package:bbblient/src/models/enums/gender.dart';
+import 'package:bbblient/src/models/enums/status.dart';
+import 'package:bbblient/src/utils/utils.dart';
+import 'package:bbblient/src/views/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -22,30 +25,45 @@ class _MyPreferencesState extends ConsumerState<MyPreferences> {
   initialisePreferences() {
     final _bnbProvider = ref.read(bnbProvider);
     _preferredCategories = _bnbProvider.customer!.preferredCategories;
+    printIt("Hi");
+    printIt(_preferredCategories.toString());
     _preferredGender = _bnbProvider.customer!.preferredGender;
+    printIt(_preferredGender);
     setState(() {});
   }
 
   String _preferredGender = PreferredGender.women;
   List<String> _preferredCategories = [];
+   Status status = Status.success;
 
   @override
   Widget build(BuildContext context) {
     final searchProvider = ref.watch(salonSearchProvider);
     final _bnbProvider = ref.watch(bnbProvider);
-    return WillPopScope(
-      onWillPop: () async {
-        await CustomerApi().updatePreferences(
-            customerId: _bnbProvider.customer!.customerId,
-            preferredGender: _preferredGender,
-            preferredCategories: _preferredCategories);
-        return true;
-      },
+   
+    return Loader(
+      status: status,
       child: Scaffold(
         appBar: AppBar(
           elevation: 0,
           leading: IconButton(
-            onPressed: () => Navigator.pop(context),
+            onPressed: () async {
+              setState(() {
+                status = Status.loading;
+              });
+              showToast(
+                 
+              AppLocalizations.of(context)?.saving ?? 'Saving...');
+              await CustomerApi().updatePreferences(
+                  customerId: _bnbProvider.customer!.customerId,
+                  preferredGender: _preferredGender,
+                  preferredCategories: _preferredCategories);
+              var _customerModel = await CustomerApi().getCustomer();
+              _bnbProvider.customer = _customerModel;
+              // status = Status.success; 
+
+              Navigator.pop(context);
+            },
             icon: const Icon(Icons.arrow_back),
           ),
           title: Text(
@@ -55,7 +73,8 @@ class _MyPreferencesState extends ConsumerState<MyPreferences> {
           centerTitle: true,
           backgroundColor: Colors.transparent,
         ),
-        body: SingleChildScrollView(
+        body: 
+        SingleChildScrollView(
           child: Column(
             children: [
               ExpansionTile(
@@ -76,6 +95,7 @@ class _MyPreferencesState extends ConsumerState<MyPreferences> {
                         value: PreferredGender.men,
                         groupValue: _preferredGender,
                         activeColor: Theme.of(context).primaryColor,
+                        //  Theme.of(context).primaryColor,
                         onChanged: (val) {
                           setState(() {
                             _preferredGender = PreferredGender.men;
@@ -83,7 +103,10 @@ class _MyPreferencesState extends ConsumerState<MyPreferences> {
                         },
                         title: Text(
                           AppLocalizations.of(context)?.forMan ?? "For Man",
-                          style: Theme.of(context).textTheme.bodyText1!.copyWith(fontWeight: FontWeight.w400),
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyText1!
+                              .copyWith(fontWeight: FontWeight.w400),
                         ),
                       ),
                       RadioListTile(
@@ -97,7 +120,10 @@ class _MyPreferencesState extends ConsumerState<MyPreferences> {
                         },
                         title: Text(
                           AppLocalizations.of(context)?.forWoman ?? "For Woman",
-                          style: Theme.of(context).textTheme.bodyText1!.copyWith(fontWeight: FontWeight.w400),
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyText1!
+                              .copyWith(fontWeight: FontWeight.w400),
                         ),
                       ),
                       RadioListTile(
@@ -110,8 +136,12 @@ class _MyPreferencesState extends ConsumerState<MyPreferences> {
                           });
                         },
                         title: Text(
-                          AppLocalizations.of(context)?.iDontCare ?? "I Don't Care",
-                          style: Theme.of(context).textTheme.bodyText1!.copyWith(fontWeight: FontWeight.w400),
+                          AppLocalizations.of(context)?.iDontCare ??
+                              "I Don't Care",
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyText1!
+                              .copyWith(fontWeight: FontWeight.w400),
                         ),
                       ),
                     ],
@@ -137,39 +167,51 @@ class _MyPreferencesState extends ConsumerState<MyPreferences> {
                     itemBuilder: (context, index) {
                       return InkWell(
                         onTap: () {
-                          if (_preferredCategories.contains(searchProvider.categories[index].categoryId)) {
+                          if (_preferredCategories.contains(
+                              searchProvider.categories[index].categoryId)) {
                             setState(() {
-                              _preferredCategories.remove(searchProvider.categories[index].categoryId);
+                              _preferredCategories.remove(
+                                  searchProvider.categories[index].categoryId);
                             });
                           } else {
                             setState(() {
-                              _preferredCategories.add(searchProvider.categories[index].categoryId);
+                              _preferredCategories.add(
+                                  searchProvider.categories[index].categoryId);
                             });
                           }
                         },
                         child: Row(
                           children: [
                             Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 4),
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 12.0, vertical: 4),
                               child: Checkbox(
-                                value: _preferredCategories.contains(searchProvider.categories[index].categoryId),
+                                value: _preferredCategories.contains(
+                                    searchProvider
+                                        .categories[index].categoryId),
                                 activeColor: Theme.of(context).primaryColor,
                                 onChanged: (val) {
                                   if (val == true) {
                                     setState(() {
-                                      _preferredCategories.add(searchProvider.categories[index].categoryId);
+                                      _preferredCategories.add(searchProvider
+                                          .categories[index].categoryId);
                                     });
                                   } else {
                                     setState(() {
-                                      _preferredCategories.remove(searchProvider.categories[index].categoryId);
+                                      _preferredCategories.remove(searchProvider
+                                          .categories[index].categoryId);
                                     });
                                   }
                                 },
                               ),
                             ),
                             Text(
-                              searchProvider.categories[index].translations[AppLocalizations.of(context)?.localeName],
-                              style: Theme.of(context).textTheme.bodyText1!.copyWith(fontWeight: FontWeight.w400),
+                              searchProvider.categories[index].translations[
+                                  AppLocalizations.of(context)?.localeName],
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyText1!
+                                  .copyWith(fontWeight: FontWeight.w400),
                             ),
                           ],
                         ),
