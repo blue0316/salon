9a9f35e9e399a89e1aa743bdaed865720c76c10f
Emmanuel <emmanuelugwueze6@gmail.com>
2022-11-07 22:03:40 +0100

    changes

--- bnb_customer/windows/flutter/generated_plugins.cmake
+++ bnb_customer/windows/flutter/generated_plugins.cmake
@@ -4,10 +4,7 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   connectivity_plus_windows
-  permission_handler_windows
-  share_plus
   url_launcher_windows
-  webview_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
