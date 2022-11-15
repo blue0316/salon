0e54bff7ee6de08dfa676beb37bfc45de4feaad9
OLUWATIMILEHIN BANJO <TIMMYBANJO@GMAIL.COM>
2022-11-15 15:22:20 +0100

    make i check

--- bnb_customer/lib/src/routes.dart
+++ bnb_customer/lib/src/routes.dart
@@ -24,7 +24,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-
+//
 import 'utils/analytics.dart';
 import 'views/salon/salon_home/salon_profile.dart';
 import 'dart:html' as html;
