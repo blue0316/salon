c6ddb4c62353b0a745c91dc5eb26bba7e30211e5
OLUWATIMILEHIN BANJO <TIMMYBANJO@GMAIL.COM>
2023-02-06 20:20:58 +0100

    alright

--- bnb_customer/functions/index.js
+++ bnb_customer/functions/index.js
@@ -7,3 +7,54 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+const express = require('express');
+const app = express();
+const cors = require('cors')({origin: true});
+app.use(cors);
+app.get('/', (req, res) => {
+    const date = new Date();
+    const hours = (date.getHours() % 12) + 1;  // London is UTC + 1hr;
+    res.send(`
+      <!doctype html>
+      <head>
+        <title>Time</title>
+        <link rel="stylesheet" href="/style.css">
+        <script src="/script.js"></script>
+      </head>
+      <body>
+        <p>In London, the clock strikes:
+          <span id="bongs">${'BONG '.repeat(hours)}</span></p>
+        <button onClick="refresh(this)">Refresh</button>
+      </body>
+    </html>`);
+  });
+//   "ignore": [
+//     "firebase.json",
+//     "**/.*",
+//     "**/node_modules/**"
+//   ],
+
+// "target1": "bnb-test-dev",
+//     "target": "bnbtest",
+//     "public": "build/web",
+
+// "functions": {
+//     "source": "functions"
+//   },
+//   "database": {
+//     "rules": "database.rules.json"
+//   },
+//   "firestore": {
+//     "rules": "firestore.rules",
+//     "indexes": "firestore.indexes.json"
+//   },
+//   "storage": {
+//     "rules": "storage.rules"
+//   }
+  app.get('/api', (req, res) => {
+    const date = new Date();
+    const hours = (date.getHours() % 12) + 1;  // London is UTC + 1hr;
+    res.json({bongs: 'BONG '.repeat(hours)});
+  });
+ 
+  exports.app = functions.https.onRequest(app);
