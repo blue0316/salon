8734dd6c06c3ba50c7d92b88a6988c69ee80436a
blayziken <kingtoluwalope@gmail.com>
2023-02-08 18:54:02 +0100

    Change asset location

--- bnb_customer/lib/src/views/themes/glam_one/core/constants/image.dart
+++ bnb_customer/lib/src/views/themes/glam_one/core/constants/image.dart
@@ -1,47 +1,47 @@
 class GlamOneImages {
-  static const longBG = 'assets/themes/images/long_bg.png';
+  static const longBG = 'assets/themes/glam_one/images/long_bg.png';
 
-  static const shortBG = 'assets/themes/images/short_bg.png';
+  static const shortBG = 'assets/themes/glam_one/images/short_bg.png';
 
-  static const lady1 = 'assets/themes/images/lady1.png';
+  static const lady1 = 'assets/themes/glam_one/images/lady1.png';
 
-  static const lady2 = 'assets/themes/images/lady2.png';
+  static const lady2 = 'assets/themes/glam_one/images/lady2.png';
 
-  static const lady3 = 'assets/themes/images/lady3.png';
+  static const lady3 = 'assets/themes/glam_one/images/lady3.png';
 
-  static const lady4 = 'assets/themes/images/lady4.png';
+  static const lady4 = 'assets/themes/glam_one/images/lady4.png';
 
-  static const makeup = 'assets/themes/images/makeup.png';
+  static const makeup = 'assets/themes/glam_one/images/makeup.png';
 
-  static const wideLadyPic = 'assets/themes/images/lady_wide.png';
+  static const wideLadyPic = 'assets/themes/glam_one/images/lady_wide.png';
 
-  static const product1 = 'assets/themes/images/product1.png';
+  static const product1 = 'assets/themes/glam_one/images/product1.png';
 
-  static const product2 = 'assets/themes/images/product2.png';
+  static const product2 = 'assets/themes/glam_one/images/product2.png';
 
-  static const product3 = 'assets/themes/images/product3.png';
+  static const product3 = 'assets/themes/glam_one/images/product3.png';
 
-  static const product4 = 'assets/themes/images/product4.png';
+  static const product4 = 'assets/themes/glam_one/images/product4.png';
 
-  static const review1 = 'assets/themes/images/review1.png';
+  static const review1 = 'assets/themes/glam_one/images/review1.png';
 
-  static const review2 = 'assets/themes/images/review2.png';
+  static const review2 = 'assets/themes/glam_one/images/review2.png';
 
-  static const review3 = 'assets/themes/images/review3.png';
+  static const review3 = 'assets/themes/glam_one/images/review3.png';
 
-  static const write1 = 'assets/themes/images/pic1.png';
+  static const write1 = 'assets/themes/glam_one/images/pic1.png';
 
-  static const write2 = 'assets/themes/images/pic2.png';
+  static const write2 = 'assets/themes/glam_one/images/pic2.png';
 
-  static const social1 = 'assets/themes/images/social1.png';
+  static const social1 = 'assets/themes/glam_one/images/social1.png';
 
-  static const social2 = 'assets/themes/images/social2.png';
+  static const social2 = 'assets/themes/glam_one/images/social2.png';
 
-  static const social3 = 'assets/themes/images/social3.png';
+  static const social3 = 'assets/themes/glam_one/images/social3.png';
 
-  static const social4 = 'assets/themes/images/social4.png';
+  static const social4 = 'assets/themes/glam_one/images/social4.png';
 
-  static const social5 = 'assets/themes/images/social5.png';
+  static const social5 = 'assets/themes/glam_one/images/social5.png';
 
-  static const map = 'assets/themes/images/map.png';
+  static const map = 'assets/themes/glam_one/images/map.png';
 }
--- bnb_customer/lib/src/views/themes/glam_one/core/constants/theme_icons.dart
+++ bnb_customer/lib/src/views/themes/glam_one/core/constants/theme_icons.dart
@@ -1,13 +1,13 @@
 class GlamOneIcons {
-  static const String insta = 'assets/themes/svg/instagram.svg';
-  static const String menu = 'assets/themes/svg/menu.svg';
-  static const String tiktok = 'assets/themes/svg/tiktok.svg';
-  static const String whatsapp = 'assets/themes/svg/whatsapp.svg';
-  static const String leftArrow = 'assets/themes/svg/left_arrow.svg';
-  static const String rightArrow = 'assets/themes/svg/right_arrow.svg';
-  static const String leftQuote = 'assets/themes/svg/left_quote.svg';
-  static const String rightQuote = 'assets/themes/svg/right_quote.svg';
-  static const String location = 'assets/themes/svg/location.svg';
-  static const String mail = 'assets/themes/svg/mail.svg';
-  static const String phone = 'assets/themes/svg/phone.svg';
+  static const String insta = 'assets/themes/glam_one/svg/instagram.svg';
+  static const String menu = 'assets/themes/glam_one/svg/menu.svg';
+  static const String tiktok = 'assets/themes/glam_one/svg/tiktok.svg';
+  static const String whatsapp = 'assets/themes/glam_one/svg/whatsapp.svg';
+  static const String leftArrow = 'assets/themes/glam_one/svg/left_arrow.svg';
+  static const String rightArrow = 'assets/themes/glam_one/svg/right_arrow.svg';
+  static const String leftQuote = 'assets/themes/glam_one/svg/left_quote.svg';
+  static const String rightQuote = 'assets/themes/glam_one/svg/right_quote.svg';
+  static const String location = 'assets/themes/glam_one/svg/location.svg';
+  static const String mail = 'assets/themes/glam_one/svg/mail.svg';
+  static const String phone = 'assets/themes/glam_one/svg/phone.svg';
 }
