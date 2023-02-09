ee55a679a2fa5daec09768004ada7e588421da60
blayziken <kingtoluwalope@gmail.com>
2023-02-09 13:14:06 +0100

    Add an onTap argument to component

--- bnb_customer/lib/src/views/themes/glam_one/core/utils/oval_button.dart
+++ bnb_customer/lib/src/views/themes/glam_one/core/utils/oval_button.dart
@@ -6,27 +6,41 @@ class OvalButton extends StatelessWidget {
   final double? height, width;
   final String text;
   final double? textSize;
-  const OvalButton({Key? key, required this.text, this.textSize, this.height, this.width}) : super(key: key);
+  final VoidCallback onTap;
+
+  const OvalButton({
+    Key? key,
+    required this.text,
+    this.textSize,
+    this.height,
+    this.width,
+    required this.onTap,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: width ?? 145.h,
-      height: height ?? 45.h,
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: GlamOneTheme.primaryColor,
-          width: 1.5,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: width ?? 145.h,
+        height: height ?? 45.h,
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: GlamOneTheme.primaryColor,
+            width: 1.5,
+          ),
+          borderRadius: const BorderRadius.all(
+            Radius.elliptical(150, 50),
+          ),
         ),
-        borderRadius: const BorderRadius.all(Radius.elliptical(150, 50)),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 3),
-        child: Center(
-          child: Text(
-            text,
-            style: GlamOneTheme.bodyText2.copyWith(
-              fontSize: textSize ?? 14.sp,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 3),
+          child: Center(
+            child: Text(
+              text,
+              style: GlamOneTheme.bodyText2.copyWith(
+                fontSize: textSize ?? 14.sp,
+              ),
             ),
           ),
         ),
