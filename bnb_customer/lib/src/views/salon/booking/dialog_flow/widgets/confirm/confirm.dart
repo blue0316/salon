f8947017c9f6732c73fde7215c92f54421071ca4
Toluwalope Oluyipe <kingtoluwalope@gmail.com>
2023-08-05 09:26:09 +0100

    Remove unused code calls

--- bnb_customer/lib/src/views/salon/booking/dialog_flow/widgets/confirm/confirm.dart
+++ bnb_customer/lib/src/views/salon/booking/dialog_flow/widgets/confirm/confirm.dart
@@ -1,7 +1,5 @@
 import 'package:bbblient/src/controller/all_providers/all_providers.dart';
 import 'package:bbblient/src/controller/create_apntmnt_provider/create_appointment_provider.dart';
-import 'package:bbblient/src/controller/salon/salon_profile_provider.dart';
-import 'package:bbblient/src/models/salon_master/salon.dart';
 import 'package:bbblient/src/utils/device_constraints.dart';
 import 'package:bbblient/src/views/salon/booking/dialog_flow/widgets/confirm/enter_number.dart';
 import 'package:flutter/material.dart';
@@ -23,9 +21,6 @@ class _ConfirmationState extends ConsumerState<Confirmation> {
   @override
   Widget build(BuildContext context) {
     final CreateAppointmentProvider _createAppointmentProvider = ref.watch(createAppointmentProvider);
-    final SalonProfileProvider _salonProfileProvider = ref.watch(salonProfileProvider);
-
-    SalonModel salonModel = _salonProfileProvider.chosenSalon;
 
     return Padding(
       padding: EdgeInsets.symmetric(
