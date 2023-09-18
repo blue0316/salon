75508aa60d9b559ffe17b5f7a24af4a886438485
Toluwalope Oluyipe <kingtoluwalope@gmail.com>
2023-09-18 11:33:48 +0100

    Services box fix

--- bnb_customer/lib/src/views/salon/default_profile_view/salon_services.dart
+++ bnb_customer/lib/src/views/salon/default_profile_view/salon_services.dart
@@ -1,5 +1,6 @@
 import 'package:bbblient/src/controller/all_providers/all_providers.dart';
 import 'package:bbblient/src/models/cat_sub_service/category_service.dart';
+import 'package:bbblient/src/models/enums/device_screen_type.dart';
 import 'package:bbblient/src/views/salon/default_profile_view/salon_profile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -32,81 +33,85 @@ class _SalonServicesState extends ConsumerState<SalonServices> {
     final ThemeData theme = _salonProfileProvider.salonTheme;
     bool isSingleMaster = _salonProfileProvider.isSingleMaster;
 
-    return SingleChildScrollView(
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height - 50.h,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            SectionSpacer(
-              title: (!isSingleMaster)
-                  ? salonTitles(
-                      AppLocalizations.of(context)?.localeName ?? 'en',
-                    )[0]
-                  : masterTitles(
-                      AppLocalizations.of(context)?.localeName ?? 'en',
-                    )[0],
-            ),
-            SizedBox(
-              // height: 1000.h,
-              width: double.infinity,
-              // color: Colors.white.withOpacity(0.7),
-              child: ListView.builder(
-                itemCount: _salonSearchProvider.categories.length,
-                shrinkWrap: true,
-                primary: false,
-                controller: _listViewController,
-                padding: const EdgeInsets.all(0),
-                physics: const NeverScrollableScrollPhysics(),
-                itemBuilder: (context, index) {
-                  if (_createAppointmentProvider.categoryServicesMap[_salonSearchProvider.categories[index].categoryId.toString()] != null && _createAppointmentProvider.categoryServicesMap[_salonSearchProvider.categories[index].categoryId.toString()]!.isNotEmpty) {
-                    final CategoryModel categoryModel = _salonSearchProvider.categories
-                        .where((
-                          element,
-                        ) =>
-                            element.categoryId == _salonSearchProvider.categories[index].categoryId.toString())
-                        .first;
+    final bool isPortrait = (DeviceConstraints.getDeviceType(MediaQuery.of(context)) == DeviceScreenType.portrait);
 
-                    return NewServiceTile(
-                      services: _createAppointmentProvider.categoryServicesMap[_salonSearchProvider.categories[index].categoryId.toString()] ?? [],
-                      categoryModel: categoryModel,
-                      listViewController: _listViewController,
-                      // initiallyExpanded: true,
-                      initiallyExpanded: _createAppointmentProvider.chosenServices
-                          .where(
-                            (element) => element.categoryId == categoryModel.categoryId,
-                          )
-                          .isNotEmpty,
-                    );
-                    // return Text("data");
-                  } else {
-                    return const SizedBox();
-                  }
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SectionSpacer(
+            title: (!isSingleMaster)
+                ? salonTitles(
+                    AppLocalizations.of(context)?.localeName ?? 'en',
+                  )[0]
+                : masterTitles(
+                    AppLocalizations.of(context)?.localeName ?? 'en',
+                  )[0],
+          ),
+          SizedBox(
+            // height: 1000.h,
+            width: double.infinity,
+            // color: Colors.white.withOpacity(0.7),
+            child: ListView.builder(
+              itemCount: _salonSearchProvider.categories.length,
+              shrinkWrap: true,
+              primary: false,
+              controller: _listViewController,
+              padding: const EdgeInsets.all(0),
+              physics: const NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                if (_createAppointmentProvider.categoryServicesMap[_salonSearchProvider.categories[index].categoryId.toString()] != null && _createAppointmentProvider.categoryServicesMap[_salonSearchProvider.categories[index].categoryId.toString()]!.isNotEmpty) {
+                  final CategoryModel categoryModel = _salonSearchProvider.categories
+                      .where((
+                        element,
+                      ) =>
+                          element.categoryId == _salonSearchProvider.categories[index].categoryId.toString())
+                      .first;
+
+                  return NewServiceTile(
+                    services: _createAppointmentProvider.categoryServicesMap[_salonSearchProvider.categories[index].categoryId.toString()] ?? [],
+                    categoryModel: categoryModel,
+                    listViewController: _listViewController,
+                    // initiallyExpanded: true,
+                    initiallyExpanded: _createAppointmentProvider.chosenServices
+                        .where(
+                          (element) => element.categoryId == categoryModel.categoryId,
+                        )
+                        .isNotEmpty,
+                  );
                   // return Text("data");
-                },
-              ),
+                } else {
+                  return const SizedBox();
+                }
+                // return Text("data");
+              },
             ),
-            Space(factor: DeviceConstraints.getResponsiveSize(context, 2, 2, 2)),
-            LandingButton(
-              color: theme.primaryColor,
-              height: 45.sp,
-              width: 250.sp,
-              borderRadius: 60.sp,
-              label: (AppLocalizations.of(context)?.bookNow ?? "Book Now").toUpperCase(),
-              fontSize: DeviceConstraints.getResponsiveSize(context, 16.sp, 20.sp, 18.sp),
-              fontWeight: FontWeight.w500,
-              textColor: Colors.white,
-              onTap: () => const BookingDialogWidget222().show(context),
-            ),
-            const Spacer(),
-            const Align(
-              alignment: Alignment.bottomCenter,
-              child: LandingBottom(),
-            ),
-          ],
-        ),
+          ),
+          Space(factor: DeviceConstraints.getResponsiveSize(context, 2, 2, 2)),
+          LandingButton(
+            color: theme.primaryColor,
+            height: 45.sp,
+            width: 250.sp,
+            borderRadius: 60.sp,
+            label: (AppLocalizations.of(context)?.bookNow ?? "Book Now").toUpperCase(),
+            fontSize: DeviceConstraints.getResponsiveSize(context, 16.sp, 20.sp, 18.sp),
+            fontWeight: FontWeight.w500,
+            textColor: Colors.white,
+            onTap: () => const BookingDialogWidget222().show(context),
+          ),
+          // Space(factor: DeviceConstraints.getResponsiveSize(context, 2, 2, 2)),
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / (isPortrait ? 3 : 4.2),
+          ),
+          // const Spacer(),
+          const Align(
+            alignment: Alignment.bottomCenter,
+            child: LandingBottom(),
+          ),
+        ],
       ),
     );
   }
